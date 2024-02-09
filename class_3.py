3c0045f1c53fbe6904945bbd3abefb0617183e3d
Dani4ka <daniilroitenberg@yandex.ru>
2024-02-09 11:15:51 +0300

    class 3

--- class_3.py
+++ class_3.py
@@ -14,5 +14,11 @@ def training(prof):
     return render_template('base1.html', title=prof, prof=prof)
 
 
+@app.route('/list_prof/<list_type>')
+def professions(list_type):
+    profs = ["слесарь", "врач", "инженер", "строитель", "программист"]
+    return render_template('base2.html', list=list_type, profs=profs)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
